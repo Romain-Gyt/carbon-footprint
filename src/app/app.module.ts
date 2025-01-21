ee8653530d89c86cdea23f4b0fd5619b2c747b70
frame <guyenot.romain@gmail.com>
2025-01-21 15:14:10 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {CommonModule, registerLocaleData} from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { CarbonFootprintComponent } from './pages/carbon-footprint/carbon-footprint.component';
+import { CarbonFootprintFormComponent } from './pages/carbon-footprint/components/carbon-footprint-form/carbon-footprint-form.component';
+import { CarbonFootprintResultComponent } from './pages/carbon-footprint/components/carbon-footprint-result/carbon-footprint-result.component';
+registerLocaleData(localeFr);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarbonFootprintComponent,
+    CarbonFootprintFormComponent,
+    CarbonFootprintResultComponent
   ],
   imports: [
     BrowserModule,
